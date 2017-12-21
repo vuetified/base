f90e67fc448cea984ef491820eb0d1354a9feb50
Uriah Galang <vuetified@gmail.com>
2017-12-22 06:37:32 +0800

    remove id preloader, added closing tag for app to avoid warning in edge

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,16 +34,16 @@
 
 @section('content')
 <div id="app" v-cloak>
-  <div id="preloader v-cloak--block full-width">
-    <div class="spinner">
-      <div class="rect1"></div>
-      <div class="rect2"></div>
-      <div class="rect3"></div>
-      <div class="rect4"></div>
-      <div class="rect5"></div>
+    <div id="v-cloak--block full-width">
+        <div class="spinner">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+            <div class="rect5"></div>
+        </div>
     </div>
-  </div>
-  <app/>
+    <app></app>
 </div>
 @endsection
 
